9d6f8003ca3e0853021ad28438732747b7aacc85
Brendon Randall <brandall@eftcorp.co.zw>
2018-08-17 10:28:32 +0100

    - Fixed some formatting - Configured webpack - Updated tns to latest version - Fixed namespace imports - Added ability to set query params for okhttp3 (considering updating this to okhttp4)

---
+++ demo/app/main-page.d.ts
@@ -0,0 +1,6 @@
+import { EventData } from 'tns-core-modules/data/observable';
+export declare function onLoaded(args: EventData): void;
+export declare function onUnloaded(args: EventData): void;
+export declare function testit(args: EventData): void;
+export declare function enableSSL(args: EventData): void;
+export declare function disableSSL(args: EventData): void;
--- https.ios.ts
+++ https.ios.ts
@@ -1,8 +1,8 @@
 // 
 
-import * as application from 'application'
-import { HttpRequestOptions, Headers, HttpResponse } from 'http'
-import { isDefined, isNullOrUndefined, isObject } from 'utils/types'
+import * as application from 'tns-core-modules/application'
+import { HttpRequestOptions, Headers, HttpResponse } from 'tns-core-modules/http'
+import { isDefined, isNullOrUndefined, isObject } from 'tns-core-modules/utils/types'
 import * as Https from './https.common'
 
 
@@ -15,39 +15,39 @@ interface Ipolicies {
 let policies: Ipolicies = {
 	def: AFSecurityPolicy.defaultPolicy(),
 	secured: false,
-}
-policies.def.allowInvalidCertificates = true
-policies.def.validatesDomainName = false
+};
+policies.def.allowInvalidCertificates = true;
+policies.def.validatesDomainName = false;
 
 export function enableSSLPinning(options: Https.HttpsSSLPinningOptions) {
 	// console.log('options', options)
 	if (!policies.secure) {
-		policies.secure = AFSecurityPolicy.policyWithPinningMode(AFSSLPinningMode.PublicKey)
-		let allowInvalidCertificates = (isDefined(options.allowInvalidCertificates)) ? options.allowInvalidCertificates : false
-		policies.secure.allowInvalidCertificates = allowInvalidCertificates
-		let validatesDomainName = (isDefined(options.validatesDomainName)) ? options.validatesDomainName : true
-		policies.secure.validatesDomainName = validatesDomainName
-		let data = NSData.dataWithContentsOfFile(options.certificate)
+		policies.secure = AFSecurityPolicy.policyWithPinningMode(AFSSLPinningMode.PublicKey);
+		let allowInvalidCertificates = (isDefined(options.allowInvalidCertificates)) ? options.allowInvalidCertificates : false;
+		policies.secure.allowInvalidCertificates = allowInvalidCertificates;
+		let validatesDomainName = (isDefined(options.validatesDomainName)) ? options.validatesDomainName : true;
+		policies.secure.validatesDomainName = validatesDomainName;
+		let data = NSData.dataWithContentsOfFile(options.certificate);
 		// console.log('data.description', data.description)
 		// console.log('data.bytes', data.bytes)
 		// console.log('data.base64Encoding()', data.base64Encoding())
 		// console.log('data.length', data.length)
 		policies.secure.pinnedCertificates = NSSet.setWithObject(data)
 	}
-	policies.secured = true
+	policies.secured = true;
 	console.log('nativescript-https > Enabled SSL pinning')
 }
 export function disableSSLPinning() {
-	policies.secured = false
+	policies.secured = false;
 	console.log('nativescript-https > Disabled SSL pinning')
 }
-console.info('nativescript-https > Disabled SSL pinning by default')
+console.info('nativescript-https > Disabled SSL pinning by default');
 
 
 
 function AFSuccess(resolve, task: NSURLSessionDataTask, data: NSDictionary<string, any> & NSData & NSArray<any>) {
 	// console.log('AFSuccess')
-	let content: any
+	let content: any;
 	if (data && data.class) {
 		// console.log('data.class().name', data.class().name)
 		if (data.enumerateKeysAndObjectsUsingBlock || data.class().name == 'NSArray') {
@@ -56,7 +56,7 @@ function AFSuccess(resolve, task: NSURLSessionDataTask, data: NSDictionary<strin
 			// 	console.log('v.description', v.description)
 			// 	content[k] = v
 			// })
-			let serial = NSJSONSerialization.dataWithJSONObjectOptionsError(data, NSJSONWritingOptions.PrettyPrinted)
+			let serial = NSJSONSerialization.dataWithJSONObjectOptionsError(data, NSJSONWritingOptions.PrettyPrinted);
 			content = NSString.alloc().initWithDataEncoding(serial, NSUTF8StringEncoding).toString()
 			// console.log('content', content)
 		} else if (data.class().name == 'NSData') {
@@ -97,8 +97,8 @@ function AFFailure(resolve, reject, task: NSURLSessionDataTask, error: NSError) 
 	// console.log('error.description', error.description)
 	// console.log('error.userInfo.description', error.userInfo.description)
 	// console.log('error.localizedDescription', error.localizedDescription)
-	let data: NSData = error.userInfo.valueForKey(AFNetworkingOperationFailingURLResponseDataErrorKey)
-	let body = NSString.alloc().initWithDataEncoding(data, NSUTF8StringEncoding).toString()
+	let data: NSData = error.userInfo.valueForKey(AFNetworkingOperationFailingURLResponseDataErrorKey);
+	let body = NSString.alloc().initWithDataEncoding(data, NSUTF8StringEncoding).toString();
 	try {
 		body = JSON.parse(body)
 	} catch (e) { }
@@ -107,7 +107,7 @@ function AFFailure(resolve, reject, task: NSURLSessionDataTask, error: NSError) 
 		description: error.description,
 		reason: error.localizedDescription,
 		url: error.userInfo.objectForKey('NSErrorFailingURLKey').description
-	}
+	};
 	// console.log('content.url', content.url)
 	// try {
 	// 	content.body = JSON.parse(body.description)
@@ -133,7 +133,7 @@ function AFFailure(resolve, reject, task: NSURLSessionDataTask, error: NSError) 
 	// if (!isNullOrUndefined(task.response)) {
 	// 	content.URL = task.response.URL
 	// }
-	let reason = error.localizedDescription
+	let reason = error.localizedDescription;
 	resolve({ task, content, reason })
 }
 
@@ -141,34 +141,34 @@ export function request(opts: Https.HttpsRequestOptions): Promise<Https.HttpsRes
 	return new Promise(function(resolve, reject) {
 		try {
 
-			let manager = AFHTTPSessionManager.manager()
+			let manager = AFHTTPSessionManager.manager();
 
 			if (opts.headers && opts.headers['Content-Type'] == 'application/json') {
-				manager.requestSerializer = AFJSONRequestSerializer.serializer()
+				manager.requestSerializer = AFJSONRequestSerializer.serializer();
 				manager.responseSerializer = AFJSONResponseSerializer.serializerWithReadingOptions(NSJSONReadingOptions.AllowFragments)
 			} else {
 				manager.requestSerializer = AFHTTPRequestSerializer.serializer()
 				// manager.responseSerializer = AFXMLParserResponseSerializer.serializer()
-				manager.responseSerializer = AFHTTPResponseSerializer.serializer()
+                // manager.responseSerializer = AFHTTPResponseSerializer.serializer()
 				// manager.responseSerializer.acceptableContentTypes = NSSet.setWithObject('text/html')
 				// manager.responseSerializer.acceptableContentTypes = NSSet.setWithObject('application/json')
 			}
-			manager.requestSerializer.allowsCellularAccess = true
-			manager.securityPolicy = (policies.secured == true) ? policies.secure : policies.def
-			manager.requestSerializer.timeoutInterval = 10
+			manager.requestSerializer.allowsCellularAccess = true;
+			manager.securityPolicy = (policies.secured == true) ? policies.secure : policies.def;
+			manager.requestSerializer.timeoutInterval = 10;
 
-			let heads = opts.headers
+			let heads = opts.headers;
 			if (heads) {
 				Object.keys(heads).forEach(function(key) {
-					manager.requestSerializer.setValueForHTTPHeaderField(heads[key] as any, key)
-				})
+					manager.requestSerializer.setValueForHTTPHeaderField(heads[key] as any, key);
+				});
 			}
 
-			let dict: NSMutableDictionary<string, any> = null
+			let dict: NSMutableDictionary<string, any> = null;
 			if (opts.body) {
-				let cont = opts.body
+				let cont = opts.body;
 				if (isObject(cont)) {
-					dict = NSMutableDictionary.new<string, any>()
+					dict = NSMutableDictionary.new<string, any>();
 					Object.keys(cont).forEach(function(key) {
 						dict.setValueForKey(cont[key] as any, key)
 					})
@@ -182,7 +182,7 @@ export function request(opts: Https.HttpsRequestOptions): Promise<Https.HttpsRes
 				'DELETE': 'DELETEParametersSuccessFailure',
 				'PATCH': 'PATCHParametersSuccessFailure',
 				'HEAD': 'HEADParametersSuccessFailure',
-			}
+			};
 			manager[methods[opts.method]](opts.url, dict, function success(task: NSURLSessionDataTask, data: any) {
 				AFSuccess(resolve, task, data)
 			}, function failure(task, error) {
@@ -216,12 +216,12 @@ export function request(opts: Https.HttpsRequestOptions): Promise<Https.HttpsRes
 		let sendi: Https.HttpsResponse = {
 			content: AFResponse.content,
 			headers: {},
-		}
+		};
 
-		let response = AFResponse.task.response as NSHTTPURLResponse
+		let response = AFResponse.task.response as NSHTTPURLResponse;
 		if (!isNullOrUndefined(response)) {
-			sendi.statusCode = response.statusCode
-			let dict = response.allHeaderFields
+			sendi.statusCode = response.statusCode;
+			let dict = response.allHeaderFields;
 			dict.enumerateKeysAndObjectsUsingBlock(function(k, v) {
 				sendi.headers[k] = v
 			})
@@ -233,22 +233,6 @@ export function request(opts: Https.HttpsRequestOptions): Promise<Https.HttpsRes
 		return Promise.resolve(sendi)
 
 	})
-
-	// {
-	//     "content": {
-	//         "code": "PreconditionFailed",
-	//         "message": "!x-uuid"
-	//     },
-	//     "statusCode": 412,
-	//     "headers": {
-	//         "Content-Length": "49",
-	//         "Server": "nginx/1.10.1",
-	//         "Content-Type": "application/json",
-	//         "Connection": "keep-alive",
-	//         "Date": "Mon, 26 Dec 2016 03:31:42 GMT"
-	//     }
-	// }
-
 }
 
 
